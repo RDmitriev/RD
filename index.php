62c29ca341e9908c65a943f2a29bc025358ce845
RDmitriev <goodchoicexxx@gmail.com>
2014-07-29 14:34:53 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Putto CSS-Framework</title>
+	<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="putto/putto.css">
+	<style>
+		body{background-color: #e5e5e5;font: 20px 'Roboto';}
+		.line{margin:0 auto;max-width:1100px;height:1000px;}
+		h1{font-size: 2.75rem;}
+		h2{font-size: 1.375rem;}
+		h3{font-size: 1.175rem;}
+		h1>sup{font-size: 20px; vertical-align:top;}
+	</style>
+</head>
+<body>
+	<div class="line">
+		<div class="row"><div class="col-1 center"><h1>Putto CSS-Framework <sup>beta</sup></h1></div></div>
+		
+		<div class="row x-right">
+			<img src="Acorn256.png">
+		</div>
+		
+		<div class="row autorow">
+			<div class="col-1 box center"><img src="Acorn256.png"><br>Тестоваое описние картинки</div>
+			<div class="col-1 box center"><img src="Acorn256.png"><br>Тестоваое описние картинкиТестоваое описние картинки</div>
+			<div class="col-1 box center"><img src="Acorn256.png"><br>Тестоваое описние картинкиТестоваое описние картинкиТестоваое описние картинки</div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+			<div class="col-1 box center"><img src="Acorn256.png"></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">
+				<img src="responsive.png">
+				<h3>Responsive</h3>
+				<p>Mobile and tablet friendly</p>
+			</div>
+			
+			<div class="col-1 box center">
+				<img src="columns.png">
+				<h3>Unlimited columns</h3>
+				Widescreen monitor? No problem!
+			</div>
+			
+			<div class="col-1 box center">
+				<img src="deployment.png">
+				<h3>Fast deployment</h3>
+				<p>Build the architecture of your site in a few minutes!</p>
+			</div>
+			
+			<div class="col-1 box center">
+				<img src="innovation.png">
+				<h3>Innovation</h3>
+				<p>Putto framework uses the latest technology CSS3 (Flexbox)</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 center"><h2>Basic Grid</h2></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 center"><h2>Hybrid Grid</h2></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">col-1</div>
+			<div class="col-9 box center">col-9</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-2 box center">col-2</div>
+			<div class="col-8 box center">col-8</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-3 box center">col-3</div>
+			<div class="col-7 box center">col-7</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-4 box center">col-4</div>
+			<div class="col-6 box center">col-6</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-5 box center">col-5</div>
+			<div class="col-5 box center">col-5</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-6 box center">col-6</div>
+			<div class="col-4 box center">col-4</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-7 box center">col-7</div>
+			<div class="col-3 box center">col-3</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-8 box center">col-8</div>
+			<div class="col-2 box center">col-2</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-9 box center">col-9</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 center"><h2>Fixed width Grid</h2></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-fix box center" style="width:200px">col-9</div>
+			<div class="col-1 box center">col-1</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 center"><h2>Form</h2></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">
+				<form>
+					<input type="text" placeholder="Text">
+					<input type="password" placeholder="Password">
+					<textarea placeholder="Textarea"></textarea>
+					<input type="submit" value="Submit">
+				</form>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 center"><h2>Table</h2></div>
+		</div>
+		
+		<div class="row">
+			<div class="col-1 box center">
+				<table>
+					<tr>
+						<td>id</td>
+						<td>Client</td>
+						<td>Status</td>
+						<td>Amount</td>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Stive</td>
+						<td>paid</td>
+						<td>$1000</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Jobs</td>
+						<td>paid</td>
+						<td>$2500</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		
+		<div class="row">
+			<footer class="col-1 center">Powered by Enfo</footer>
+		</div>
+	</div>
+	
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44165145-2', 'putto.ru');
+ga('send', 'pageview');
+</script>
+</body>
+</html>
