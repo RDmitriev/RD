fddea04a510eabdc70d35b6272ecd2fb205d3600
RDmitriev <goodchoicexxx@gmail.com>
2014-07-29 14:54:40 +0300

    delete google GoogleAnalytics

--- index.php
+++ index.php
@@ -241,15 +241,5 @@
 			<footer class="col-1 center">Powered by Enfo</footer>
 		</div>
 	</div>
-	
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-44165145-2', 'putto.ru');
-ga('send', 'pageview');
-</script>
 </body>
 </html>
