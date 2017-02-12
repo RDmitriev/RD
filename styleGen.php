3ad560737d3286fc07792bef0dfc5017af84f0ac
Ruslan Dmitriev <goodchoicexxx@gmail.com>
2017-02-12 20:39:12 +0300

    генератор болванок

---
+++ styleGen.php
@@ -0,0 +1,26 @@
+<?
+	/*
+		RD CSS/JS Framework + Normalize + Blank
+		
+		Powered by Руслан Enfo Дмитриев
+		http://rdmitriev.ru/githab/RD
+		https://github.com/RDmitriev/RD
+	*/
+	
+	for($i=1; $i <= 20; $i++)
+	{
+		echo '.box-' . $i . '{background: url(img/bg' . $i . '.jpg) top center no-repeat;background-size:cover;}
+.box-' . $i . ' .line-1{} .box-' . $i . ' .line-1 a{}
+.box-' . $i . ' .line-2{} .box-' . $i . ' .line-2 a{}
+.box-' . $i . ' .line-3{} .box-' . $i . ' .line-3 a{}
+.box-' . $i . ' .line-4{} .box-' . $i . ' .line-4 a{}
+.box-' . $i . ' .line-5{} .box-' . $i . ' .line-5 a{}
+.box-' . $i . ' .line-6{} .box-' . $i . ' .line-6 a{}
+.box-' . $i . ' .line-7{} .box-' . $i . ' .line-7 a{}
+.box-' . $i . ' .line-8{} .box-' . $i . ' .line-8 a{}
+.box-' . $i . ' .line-9{} .box-' . $i . ' .line-9 a{}
+.box-' . $i . ' .line-10{} .box-' . $i . ' .line-10 a{}
+
+';
+	}
+?>
